3e75162e33b0569ed99bf05381dad1741a3277d3
pedro-donoso <pedro.donoso.r@usach.cl>
2024-06-03 17:01:23 -0400

    raiz cuadrada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("¡Hola mundo!");
+        System.out.printf("Ingrese un número: ");
+        Scanner sc = new Scanner(System.in);
+        long numero = sc.nextLong();
+        double result = Math.sqrt(numero);
+        System.out.printf("La raíz cuadradad es: %f", result);
+        sc.close();
     }
 }
