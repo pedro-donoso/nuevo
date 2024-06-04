36998dc31cd5994c6dc2f54c5c9d7a4a116e3ad5
pedro-donoso <pedro.donoso.r@usach.cl>
2024-06-03 23:23:03 -0400

    agrego imagen

--- src/App.java
+++ src/App.java
@@ -4,15 +4,15 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Ingresa la distancia en km: ");
-        float distancia = sc.nextFloat();
+        System.out.println("¿Qué edad tienes?");
+        int edad = sc.nextInt();
+        if(edad >= 18) {
+        System.out.println("Eres mayor de edad");
+        }
+        if(edad < 18) {
+        System.out.println("Eres menor de edad");
+        }
         
-        System.out.print("Ingresa el tiempo en horas: ");
-        float tiempo = sc.nextFloat();
-        
-        float velocidad = distancia / tiempo;
-        
-        System.out.printf("La velocidad es: %.2f km/h%n", velocidad);
         
         sc.close();
     }
