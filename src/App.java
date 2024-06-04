fe281b133ca914b5b42e9ad8220b0587061a51a7
pedro-donoso <pedro.donoso.r@usach.cl>
2024-06-03 22:44:20 -0400

    nombre

--- src/App.java
+++ src/App.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.printf("a + b es igual a %d \n", a+b);
-        System.out.printf("a * b es igual a %d \n", a*b);
+        int edad = 34;
+        String nombre = "William";
+        String salida = String.format("%s tiene %d años.", nombre, edad);
+        System.out.println(salida);
         sc.close();
     }
 }
